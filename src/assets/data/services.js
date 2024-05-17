72ff18fe1145079192c9e9471549af78fe9a9993
Mariodev27 <mariosantisteban23@gmail.com>
2024-05-17 01:03:58 -0500

    frontend

---
+++ src/assets/data/services.js
@@ -0,0 +1,20 @@
+export const services = [
+  {
+    name: "Cancer Care",
+    desc: "Personalized treatment plans and unwavering support for every step of your journey.",
+    bgColor: "rgba(254, 182, 13, .2)",
+    textColor: "#FEB60D",
+  },
+  {
+    name: "Labor & Delivery",
+    desc: "Expert support and nurturing care for a safe and joyous childbirth experience.",
+    bgColor: "rgba(151, 113, 255, .2)",
+    textColor: "#9771FF",
+  },
+  {
+    name: "Heart & Vascular",
+    desc: "State-of-the-art diagnostics and innovative treatments to safeguard your heart's health.",
+    bgColor: "rgba(1, 181, 197, .2)",
+    textColor: "#01B5C5",
+  },
+];
